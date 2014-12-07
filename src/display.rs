aa30d526876cc4021a5612e05f804c385c6a7247
Nathan Typanski <ntypanski@gmail.com>
2014-12-07 02:15:54 -0500

    update for namespaced enum changes

--- src/display.rs
+++ src/display.rs
@@ -8,7 +8,7 @@ pub fn screen_init() -> (i32, i32) {
     ncurses::raw();
     ncurses::keypad(ncurses::stdscr, true);
     ncurses::noecho();
-    ncurses::curs_set(ncurses::CURSOR_INVISIBLE);
+    ncurses::curs_set(ncurses::CURSOR_VISIBILITY::CURSOR_INVISIBLE);
     ncurses::start_color();
     ncurses::init_pair(1, ncurses::COLOR_WHITE, ncurses::COLOR_WHITE);
     ncurses::init_pair(2, ncurses::COLOR_BLUE, ncurses::COLOR_BLUE);
